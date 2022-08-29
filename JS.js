2e15c17007131a5c62e81bebf7bfe477a240c068
SANTOS246 <77444766+santos246@users.noreply.github.com>
2022-08-29 19:06:47 -0300

    Create JS.js

---
+++ JS.js
@@ -0,0 +1,8 @@
+﻿$("#tabs").tabs();
+
+$(".proximo").on("click", function() 
+{
+    var indice = $('#tabs ul li a[href="#' + $(this).parent().prop("id") + '"]').parent().index();
+    
+    $("#tabs").tabs("option", "active", (indice + 1));
+});
